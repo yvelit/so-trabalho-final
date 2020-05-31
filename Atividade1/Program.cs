4b254ad489e524400b36ac595c5901363917df63
Yuske Eduardo Velit Arruda <yuske.velit@totvs.com.br>
2020-05-31 18:00:11 -0300

    Init

---
+++ Atividade1/Program.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Threading;
+
+namespace Atividade1
+{
+    internal class Program
+    {
+        private static Filosofo filosofo1 = new Filosofo();
+
+        private static Filosofo filosofo2 = new Filosofo();
+
+        private static Filosofo filosofo3 = new Filosofo();
+
+        private static Filosofo filosofo4 = new Filosofo();
+
+        private static Filosofo filosofo5 = new Filosofo();
+
+        private static Garfo garfo1 = new Garfo();
+
+        private static Garfo garfo2 = new Garfo();
+
+        private static Garfo garfo3 = new Garfo();
+
+        private static Garfo garfo4 = new Garfo();
+
+        private static Garfo garfo5 = new Garfo();
+
+        private static Random rand = new Random();
+
+        private static void Main(string[] args)
+        {
+            Thread threadPrincipal = new Thread(new ThreadStart(Start));
+
+            threadPrincipal.Start();
+
+            while (true)
+
+            {
+                SituacaoDaMesa();
+
+                Thread.Sleep(3000);
+            }
+        }
+
+        private static void SituacaoDaMesa()
+
+        {
+            string situacaoFilosofo1 = filosofo1.EstaPensando ? "PENSANDO" : "COMENDO";
+
+            Console.WriteLine(string.Format("Filósofo 1 está: {0}", situacaoFilosofo1));
+
+            string situacaoFilosofo2 = filosofo2.EstaPensando ? "PENSANDO" : "COMENDO";
+
+            Console.WriteLine(string.Format("Filósofo 2 está: {0}", situacaoFilosofo2));
+
+            string situacaoFilosofo3 = filosofo3.EstaPensando ? "PENSANDO" : "COMENDO";
+
+            Console.WriteLine(string.Format("Filósofo 3 está: {0}", situacaoFilosofo3));
+
+            string situacaoFilosofo4 = filosofo4.EstaPensando ? "PENSANDO" : "COMENDO";
+
+            Console.WriteLine(string.Format("Filósofo 4 está: {0}", situacaoFilosofo4));
+
+            string situacaoFilosofo5 = filosofo5.EstaPensando ? "PENSANDO" : "COMENDO";
+
+            Console.WriteLine(string.Format("Filósofo 5 está: {0}", situacaoFilosofo5));
+
+            Console.WriteLine("");
+        }
+
+        private static void Start()
+        {
+            Thread threadFilosofo1 = new Thread(new ThreadStart(Filosofo1Come));
+
+            Thread threadFilosofo2 = new Thread(new ThreadStart(Filosofo2Come));
+
+            Thread threadFilosofo3 = new Thread(new ThreadStart(Filosofo3Come));
+
+            Thread threadFilosofo4 = new Thread(new ThreadStart(Filosofo4Come));
+
+            Thread threadFilosofo5 = new Thread(new ThreadStart(Filosofo5Come));
+
+            while (true)
+
+            {
+                if (!threadFilosofo1.IsAlive)
+
+                {
+                    threadFilosofo1 = new Thread(new ThreadStart(Filosofo1Come));
+
+                    threadFilosofo1.Start();
+                }
+
+                if (!threadFilosofo2.IsAlive)
+
+                {
+                    threadFilosofo2 = new Thread(new ThreadStart(Filosofo2Come));
+
+                    threadFilosofo2.Start();
+                }
+
+                if (!threadFilosofo3.IsAlive)
+
+                {
+                    threadFilosofo3 = new Thread(new ThreadStart(Filosofo3Come));
+
+                    threadFilosofo3.Start();
+                }
+
+                if (!threadFilosofo4.IsAlive)
+
+                {
+                    threadFilosofo4 = new Thread(new ThreadStart(Filosofo4Come));
+
+                    threadFilosofo4.Start();
+                }
+
+                if (!threadFilosofo5.IsAlive)
+
+                {
+                    threadFilosofo5 = new Thread(new ThreadStart(Filosofo5Come));
+
+                    threadFilosofo5.Start();
+                }
+            }
+        }
+
+        private static void Filosofo1Come()
+        {
+            if (garfo1.EstaEmUso || garfo5.EstaEmUso)
+
+                return;
+
+            garfo1.EstaEmUso = true;
+
+            garfo5.EstaEmUso = true;
+
+            int tempoComendo = rand.Next(2000, 5000);
+
+            filosofo1.EstaPensando = false;
+
+            Thread.Sleep(tempoComendo);
+
+            filosofo1.EstaPensando = true;
+
+            garfo1.EstaEmUso = false;
+
+            garfo5.EstaEmUso = false;
+        }
+
+        private static void Filosofo2Come()
+        {
+            if (garfo1.EstaEmUso || garfo2.EstaEmUso)
+
+                return;
+
+            garfo1.EstaEmUso = true;
+
+            garfo2.EstaEmUso = true;
+
+            int tempoComendo = rand.Next(2000, 5000);
+
+            filosofo2.EstaPensando = false;
+
+            Thread.Sleep(tempoComendo);
+
+            filosofo2.EstaPensando = true;
+
+            garfo1.EstaEmUso = false;
+
+            garfo2.EstaEmUso = false;
+        }
+
+        private static void Filosofo3Come()
+        {
+            if (garfo2.EstaEmUso || garfo3.EstaEmUso)
+
+                return;
+
+            garfo2.EstaEmUso = true;
+
+            garfo3.EstaEmUso = true;
+
+            int tempoComendo = rand.Next(2000, 5000);
+
+            filosofo3.EstaPensando = false;
+
+            Thread.Sleep(tempoComendo);
+
+            filosofo3.EstaPensando = true;
+
+            garfo2.EstaEmUso = false;
+
+            garfo3.EstaEmUso = false;
+        }
+
+        private static void Filosofo4Come()
+        {
+            if (garfo3.EstaEmUso || garfo4.EstaEmUso)
+
+                return;
+
+            garfo3.EstaEmUso = true;
+
+            garfo4.EstaEmUso = true;
+
+            int tempoComendo = rand.Next(2000, 5000);
+
+            filosofo4.EstaPensando = false;
+
+            Thread.Sleep(tempoComendo);
+
+            filosofo4.EstaPensando = true;
+
+            garfo3.EstaEmUso = false;
+
+            garfo4.EstaEmUso = false;
+        }
+
+        private static void Filosofo5Come()
+        {
+            if (garfo4.EstaEmUso || garfo5.EstaEmUso)
+
+                return;
+
+            garfo4.EstaEmUso = true;
+
+            garfo5.EstaEmUso = true;
+
+            int tempoComendo = rand.Next(2000, 5000);
+
+            filosofo5.EstaPensando = false;
+
+            Thread.Sleep(tempoComendo);
+
+            filosofo5.EstaPensando = true;
+
+            garfo4.EstaEmUso = false;
+
+            garfo5.EstaEmUso = false;
+        }
+
+        public class Filosofo
+        {
+            public Filosofo()
+            {
+                EstaPensando = true;
+            }
+
+            private bool _estaPensando;
+
+            public bool EstaPensando
+            {
+                get
+                {
+                    return _estaPensando;
+                }
+                set
+                {
+                    _estaPensando = value;
+                }
+            }
+        }
+
+        public class Garfo
+        {
+            public Garfo()
+
+            {
+                EstaEmUso = false;
+            }
+
+            private bool _estaPensando;
+
+            public bool EstaEmUso
+            {
+                get
+                { return _estaPensando; }
+                set { _estaPensando = value; }
+            }
+        }
+    }
+}
---
+++ Atividade5/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("CALCULADORA");
+                Console.WriteLine();
+
+                var argumentos = new Argumentos(args);
+
+                decimal resultado;
+
+                switch (argumentos.Operacao)
+                {
+                    case Operacao.Soma:
+                        resultado = Calculadora.Soma(argumentos.X, argumentos.Y);
+                        break;
+
+                    case Operacao.Subtracao:
+                        resultado = Calculadora.Subtrai(argumentos.X, argumentos.Y);
+                        break;
+
+                    case Operacao.Multiplicacao:
+                        resultado = Calculadora.Multiplica(argumentos.X, argumentos.Y);
+
+                        break;
+
+                    case Operacao.Divisao:
+                        resultado = Calculadora.Divide(argumentos.X, argumentos.Y);
+
+                        break;
+
+                    default:
+                        resultado = 0;
+                        break;
+                }
+
+                Console.WriteLine("Resultado: " + args[0] + " " + args[1] + " " + args[2] + " = " + resultado);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("Pressione qualquer tecla para fechar a calculadora");
+                Console.ReadKey();
+            }
+        }
+
+        public enum Operacao
+        {
+            Soma,
+            Subtracao,
+            Multiplicacao,
+            Divisao,
+        }
+
+        public static class Calculadora
+        {
+            public static decimal Soma(decimal x, decimal y)
+            {
+                return x + y;
+            }
+
+            public static decimal Subtrai(decimal x, decimal y)
+            {
+                return x - y;
+            }
+
+            public static decimal Multiplica(decimal x, decimal y)
+            {
+                return x * y;
+            }
+
+            public static decimal Divide(decimal x, decimal y)
+            {
+                if (y == 0)
+                {
+                    throw new InvalidOperationException("Não é possível fazer divisão por zero");
+                }
+                return x / y;
+            }
+        }
+
+        public class Argumentos
+        {
+            private decimal _x;
+
+            public decimal X
+            {
+                get
+                {
+                    return _x;
+                }
+                set
+                {
+                    _x = value;
+                }
+            }
+
+            private decimal _y;
+
+            public decimal Y
+            {
+                get
+                {
+                    return _y;
+                }
+                set
+                {
+                    _y = value;
+                }
+            }
+
+            private Operacao _operacao;
+
+            public Operacao Operacao
+            {
+                get
+                {
+                    return _operacao;
+                }
+                set
+                {
+                    _operacao = value;
+                }
+            }
+
+            public Argumentos(string[] args)
+            {
+                if (args == null || args.Length < 3)
+                {
+                    throw new ArgumentException("A calculadora precisa de 3 argumentos");
+                }
+
+                decimal x;
+
+                try
+                {
+                    X = decimal.Parse(args[0]);
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentException("Argumento 1 deve ser um número.");
+                }
+                var ehDecimal = decimal.TryParse(args[0], out x);
+
+                switch (args[1])
+                {
+                    case "+":
+                        Operacao = Operacao.Soma;
+                        break;
+
+                    case "-":
+                        Operacao = Operacao.Subtracao;
+                        break;
+
+                    case "x":
+                        Operacao = Operacao.Multiplicacao;
+                        break;
+
+                    case "/":
+                        Operacao = Operacao.Divisao;
+                        break;
+
+                    default:
+                        throw new ArgumentException("Argumento 2 deve ser + ou - ou x ou /.");
+                }
+
+                try
+                {
+                    Y = decimal.Parse(args[2]);
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentException("Argumento 3 deve ser um número.");
+                }
+
+                decimal y;
+                ehDecimal = decimal.TryParse(args[2], out y);
+
+                if (!ehDecimal)
+                {
+                }
+
+                Y = y;
+            }
+        }
+    }
+}
