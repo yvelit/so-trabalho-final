6b1fcde3cde4824d01a658ebecf91ec4d9e4f5d2
Luiz Henrique Gomes <luiz.gomes@portalssp.com>
2020-06-02 12:06:57 -0300

    Atividade 2 - Barbeiro adormecido

---
+++ Atividade2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2
+{
+    class Program
+    {
+		public static void Main(String[] args)
+		{
+
+
+			Barbeiro barbeiro = new Barbeiro(2, 10); // Construtor recebe por parametro
+														// a quantidade de cadeiras de espera
+														// e a quantidade de clientes que podem chegar
+														// na barbearia
+			Thread threadBarbeiro = new Thread(barbeiro.run);
+			threadBarbeiro.Start();
+
+		}
+	}
+    
+}
